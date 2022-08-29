27aaf0508b6252f98cf73f98b0b2057fe6dbc3ba
ghayas9 <ghayasudin999@gmail.com>
2022-08-29 20:38:23 +0500

    nx

--- src/Components/TopBar/SubBar.js
+++ src/Components/TopBar/SubBar.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import logo from '../Image/logo.png'
 
 const SubBar = ()=>{
+    const [show ,setShow] = useState(true)
     return(
         <div  className='subbar'>
             <img src={logo} alt="" />
             <div  className='subbarin'>
-                <div style={{padding:'0px 20px'}}>BUY {' >'}</div>
+                <div style={{padding:'0px 20px',cursor:'pointer'}} onClick={()=>{
+                    setShow(!show)
+                }}>BUY {' >'}</div>
                 <div  className='subbarm'>
-                    <div>HOME</div>
-                    <div>PLOTS</div>
-                    <div>COMMERCAIL</div>
+                    {
+                        show?<>
+                        <div>HOME</div>
+                        <div>PLOTS</div>
+                        <div>COMMERCAIL</div>
+                        </>:null
+                    }
 
                     <div className="subm">
                         <div>RENT</div>
